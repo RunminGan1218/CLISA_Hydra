02a9982398f0fa95262d488af50bfa1c41111161
RunminGan1218 <354613146@qq.com>
2024-05-13 13:27:15 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,17 +1,18 @@
-run=8
+run=11
 dataset=FACED
 gpus='[0]'
 proj_name="$dataset""_test"
 valid_method=10
 logging=default
 iftest=True
-# # first run this, do not run the second step
-# echo "proj: $proj_name run: $run gpus: $gpus valid_method: $valid_method"
-# python train_ext.py log.run=$run log.proj_name=$proj_name data=$dataset \
-#                     train.gpus=$gpus train.valid_method=$valid_method \
-#                     hydra/job_logging=$logging train.iftest=$iftest
 
-# second run this, before running config the checkpoint path, and don't run first step
+# you can run it in once
+echo "proj: $proj_name run: $run gpus: $gpus valid_method: $valid_method"
+python train_ext.py log.run=$run log.proj_name=$proj_name data=$dataset \
+                    train.gpus=$gpus train.valid_method=$valid_method \
+                    hydra/job_logging=$logging train.iftest=$iftest
+
+
 python extract_fea.py log.run=$run log.proj_name=$proj_name data=$dataset \
                       train.gpus=$gpus train.valid_method=$valid_method \
                       hydra/job_logging=$logging train.iftest=$iftest
