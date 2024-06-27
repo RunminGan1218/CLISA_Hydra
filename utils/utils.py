c0d843bfe7795fea498af3f7b5112d351c1f8c0b
RunminGan1218 <354613146@qq.com>
2024-06-27 23:20:49 +0800

    update confusion mat related code.

--- utils/utils.py
+++ utils/utils.py
@@ -12,7 +12,7 @@ from gymnasium.spaces import MultiDiscrete, Discrete, Box
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 
 
-def config_logging(log_file="main.log"):
+def config_logging(log_file="main.log", log_level=logging.INFO):
     date_format = '%Y-%m-%d %H:%M:%S'
     log_format = '%(asctime)s: [%(levelname)s]: %(message)s'
     stdout_handler = logging.StreamHandler(sys.stdout)
@@ -21,7 +21,7 @@ def config_logging(log_file="main.log"):
     # Set up the FileHandler for logging to a file
     file_handler = logging.FileHandler(log_file, mode='a')
     file_handler.setFormatter(logging.Formatter(log_format, datefmt=date_format))
-    logging.basicConfig(level=logging.INFO, handlers=[stdout_handler, file_handler])
+    logging.basicConfig(level=log_level, handlers=[stdout_handler, file_handler])
     
 def set_seed_everywhere(env: gym.Env, seed=0):
     env.action_space.seed(seed)
@@ -33,12 +33,19 @@ def set_seed_everywhere(env: gym.Env, seed=0):
     # torch.backends.cudnn.benchmark = False
     # pl.seed_everything(cfg.seed) #equal to fist 3 lines and it can pass seeds
     
-def get_confusionMat(output, target, n_class):
-    with torch.no_grad():
-        _, pred = output.topk(1, 1, True, True)
-        pred = pred.t()
-        confusionMat = torch.zeros((n_class, n_class))
+def get_confusionMat(pred, target, n_class):
+    if isinstance(pred, torch.Tensor) and isinstance(target, torch.Tensor):
+        with torch.no_grad():
+            # _, pred = output.topk(1, 1, True, True)
+            # pred = pred.t()
+            confusionMat = torch.zeros((n_class, n_class))
+            for i in range(n_class):
+                for j in range(n_class):
+                    confusionMat[i, j] = torch.sum((pred==i) & (target==j))
+            return confusionMat
+    else:
+        confusionMat = np.zeros((n_class, n_class), dtype=int)
         for i in range(n_class):
             for j in range(n_class):
-                confusionMat[i, j] = torch.sum((pred==i) & (target==j))
+                confusionMat[i, j] = np.sum((pred == i) & (target == j))
         return confusionMat
